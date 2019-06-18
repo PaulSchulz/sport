057ec2187a9b98c3fc754284485a0c0a9bafe688
Paul Schulz <paul@mawsonlakes.org>
2019-06-18 10:31:57 +0930

    Updates FIFA Womens World Cup 2019 results
    Also, fixes bug in timezone calculations, and timezone data.
    - Times were originally given as +0100 timezone in calendar file,
      but France is on Summer Time.
    - Current using an older time manipuation library (clj-time).
    - Added table output of Group state statistics

--- src/sporting_fixtures/core.clj
+++ src/sporting_fixtures/core.clj
@@ -1,7 +1,11 @@
 (ns sporting-fixtures.core
   (:gen-class)
   (:require [clj-yaml.core :as yaml]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clj-time.core   :as t]
+            [clj-time.format :as f]
+            [clj-time.local  :as l]
+            )
   (:use [clojure.java.shell :only [sh]]
         [clojure.pprint :only [pprint]])
   )
@@ -15,15 +19,28 @@
   (println
    (str/join
     "\n"
-    ["==============="
-     "Useful Commands"
-     "==============="
+    [";;;;;;;;;;;;;;;;;;;"
+     ";; Useful Commands"
+     ";;;;;;;;;;;;;;;;;;;"
      "(list-events)                - Show current event files"
      "(read-event \"<filename>\")    - Load event details" 
-     "(display-event \"<filename>\") - Display evenmt details"
+     "(display-event \"<filename>\") - Display event details"
      "(events-table)               - List events"
+     ""
+     ";; Examples"
+     "  (display-event (event-id-to-filename \"2019-fra-fifa-women-worldcup\"))"
+     ""
+     ";; FIFA 2019 Womens World Cup"
+     "  (event-games nil)"
+     "  (println (event-games-table (event-games)))"
+     "  (println (event-stats-table (event-games)))"
+     "  (println (event-group-table (get-event)))"
      ]
     )))
+
+(defn event-id-to-filename [id]
+  (str "data/" id ".yml"))
+
 
 (defn get-events []
   (str/split (:out (sh "bash" "-c" "ls data/20*.yml")) #"\n"))
@@ -58,4 +75,260 @@
                 (str/join "\n" (map #(event-details fmt %) (get-events)))
                 ]))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 2019 FIFA Womens World Cup
+;; fifa-2019
+(defn index-of [v i] (.indexOf v i))
 
+(def  event-name  "2019-fra-fifa-women-worldcup")
+(defn get-event   [] (read-event (event-id-to-filename event-name)))
+(defn event-games [] (:games (get-event)))
+
+
+;; Consider changing to 'clojure-java-time'
+;;   See: https://github.com/dm3/clojure.java-time
+;; Time parsing an manipulation
+;; Formatters can be listed with: (f/show-formatters)
+(defn localtime [datetime]
+  (let [formatter (f/formatter :rfc822)
+        my-formatter (f/with-zone
+                       (f/formatter "EE dd hh:mm aa")
+                       (t/default-time-zone))
+        ]
+    (f/unparse my-formatter
+               (f/parse formatter datetime))
+               
+    ))
+
+;; Create results table 
+(defn event-games-table [games]
+  (str/join
+   "\n"
+   (map
+    (fn [x]
+      (format " %2d | %s | %-10s |  %4s %4s | %s"
+              (:id x)
+              (localtime (:time x))
+              (str (if (nth (:teams x) 0)
+                     (str/upper-case (nth (:teams x) 0))
+                     "---")
+                   " vs "
+                   (if (nth (:teams x) 1)
+                     (str/upper-case (nth (:teams x) 1))
+                     "---"))
+              (str
+               (if (nth (:goals x) 0)
+                 (nth (:goals x) 0)
+                 "-")
+               (if (nth (:penalties x) 0)
+                 (str "/" (nth (:penalties x) 0))
+                 "  "))
+              (str
+               (if (nth (:goals x) 1)
+                 (nth (:goals x) 1)
+                 "-")
+               (if (nth (:penalties x) 1)
+                 (str "/" (nth (:penalties x) 1))
+                 "  "))
+              (if (:result x) (:result x) "")
+              ))
+    games)
+   )
+  )
+
+;; goals - vector containing number of goals for each team.
+;; No need to consider penalties and points are not awarded for matches
+;; where a penalty shootout is required (in knockout rounds).
+(defn calculate-points [goals]
+  (let [a-goals (nth goals 0)
+        b-goals (nth goals 1)]
+
+    (cond
+      (not a-goals)       nil
+      (not b-goals)       nil
+      (> a-goals b-goals) [3 0]
+      (< a-goals b-goals) [0 3]
+      :else               [1 1]
+      )
+    ))
+
+;; goals - vector containing number of goals for each team.
+;; No need to consider penalties and points are not awarded for matches
+;; where a penalty shootout is required (in knockout rounds).
+(defn calculate-stats [goals]
+  (let [a-goals (nth goals 0)
+        b-goals (nth goals 1)]
+
+    (cond
+      (not a-goals)       nil
+      (not b-goals)       nil
+      (> a-goals b-goals) [[3 1 1 0 0 a-goals b-goals (- a-goals b-goals)]
+                           [0 1 0 1 0 b-goals a-goals (- b-goals a-goals)]]
+      (< a-goals b-goals) [[0 1 0 1 0 a-goals b-goals (- a-goals b-goals)]
+                           [3 1 1 0 0 b-goals a-goals (- b-goals a-goals)]]
+      :else               [[1 1 0 0 1 a-goals b-goals (- a-goals b-goals)]
+                           [1 1 0 0 1 b-goals a-goals (- b-goals a-goals)]]
+      )
+    ))
+
+;; goals     - vector containing number of goals
+;;             scored by each team.
+;; penalties - vector containing number of penalties
+;;             scored by each team. Can be 'nil' if the
+;;             there is a goal difference.
+(defn calculate-winner [goals penalties]
+  (let [a-goals     (nth goals 0)
+        b-goals     (nth goals 1)
+        ;; Check in case penalties is 'nil'
+        sane-penalties (if penalties penalties [0 0])
+        a-penalties (nth sane-penalties 0)
+        b-penalties (nth sane-penalties 1)
+        ]
+
+    (cond
+      (not a-goals)               nil
+      (not b-goals)               nil
+      (> a-goals b-goals)         [1 0]
+      (< a-goals b-goals)         [0 1]
+      ;; Assert: Number of goals scors is the same for each team.
+      (not a-penalties)           [0 0]
+      (not b-penalties)           [0 0]
+      (> a-penalties b-penalties) [1 0]
+      (< a-penalties b-penalties) [0 1]
+      :else                       [0 0]
+      )))
+  
+(defn calculate-statistics [games]
+  (map
+   (fn [x]
+     (let [team-a (nth (:teams x) 0)
+           team-b (nth (:teams x) 1)
+           points (calculate-points (:goals x))
+           stats  (calculate-stats  (:goals x))
+           winner (calculate-winner (:goals x) (:penalties x))
+           ]
+       (conj {}
+             (if team-a
+               {(keyword team-a) (nth stats 0)}
+               nil)
+             (if team-b
+               {(keyword team-b) (nth stats 1)}
+               nil)
+             )
+      ))
+   games)
+  )
+
+(defn reduce-statistics [stats]
+  (reduce (fn [val coll]
+            (reduce conj val coll))
+          [] stats)
+  )
+
+;; statistics - Map of team statistics
+;; update     - Array with 'team id' and 'statistics update'
+(defn update-statistics [statistics update]
+  (let [team         (nth update 0)
+        stats-update (if (nth update 1)
+                       (nth update 1)
+                       [0 0 0 0 0 0 0 0])
+        stats-old    (if (team statistics)
+                       (team statistics)
+                       [0 0 0 0 0 0 0 0])]
+
+    (conj statistics {team (map + stats-old stats-update)}))
+  )
+
+(defn event-statistics [games]
+  (reduce update-statistics {}
+          (reduce-statistics
+           (calculate-statistics games))))
+
+(defn stats-header []
+  (str
+   " Team | Points | Played/Win/Lose/Draw | For/Against/Diff\n"
+   "------+--------+----------------------+-----------------\n"
+   )
+  )
+
+(defn stats-separator []
+  "------+--------+----------------------+-----------------\n"
+  )
+
+(defn stats-string [team stats]
+  (format " %3s  |  %4d  |   %3d %3d %3d %3d    |  %3d %3d %4d"
+          (str/upper-case (name team))
+          (nth stats 0)
+          (nth stats 1)
+          (nth stats 2)
+          (nth stats 3)
+          (nth stats 4)
+          (nth stats 5)
+          (nth stats 6)
+          (nth stats 7)
+          ))
+
+(defn event-stats-table [games]
+  (let [statistics (event-statistics games)]
+    (str
+     "\n"
+     (stats-header)
+     (str/join
+      "\n"
+      (map
+       (fn [[k v]] (stats-string k v))
+       (sort (fn [el1 el2]
+               (if (> (nth (nth el1 1) 0)
+                      (nth (nth el2 1) 0))
+                 true
+                 false))
+        (map (fn [x] x) statistics))
+       )
+      )
+    ))
+  )
+
+(defn team-group [event team]
+  (let [groups (:groups event)]        
+    (str team
+         (seq
+          groups
+          )
+
+         )
+    )
+  )
+  
+(defn event-group-table [event]
+  (let [games  (:games event)
+        groups (:groups event)
+        stats  (event-statistics games)]
+
+    (str
+     (stats-header)
+     (str/join
+      (str
+       "\n"
+       (stats-separator)
+       )
+       (map (fn [group group-details]
+              (str/join
+               "\n"
+               (map
+                (fn [x]
+                  (let [team (keyword x)]
+                    (stats-string team (team stats))
+                    ;; (str team
+                    ;;      " | "
+                    ;;      (seq (team stats))
+                    ;;      "\n")
+                    )
+                  )
+                group)))
+            (:groups (get-event))
+            (:group-details (get-event))
+          ))
+     )
+    )
+  )
+  
