7bd9c62ba3ad0e45f88f2365fa56c6558f4a891c
Paul Schulz <paul@mawsonlakes.org>
2023-01-06 21:06:33 +1030

    bbl-2022: Added incomplete ball-by-ball report for match-31

---
+++ data/bbl-2022/match-31-INCOMPLETE.clj
@@ -0,0 +1,27 @@
+;;
+[[:format "bbl-2023-01-04"]
+ [:batting-team "Stars"
+  :bowling-team "Sixers"]
+ [:batsmen "Clarke" "Tim Rogers"]
+
+ [:bowler "Abbott" 1]
+ [1 1 "<" "."]
+ [1 2 "<" "2"]
+ [1 3 "<" "."]
+ [1 4 "<" "."]
+ [1 5 "<" "4"]
+ [1 6 "<" "."]
+ [:score [6 0]]
+
+ [:bowler "Murphy" 1]
+ [2 1 ">" "1"]
+ [2 2 "<" "1"]
+ [2 3 ">" "6"]
+ [2 4 ">" "."]
+ [2 5 ">" "1"]
+ [2 6 "<" "1"]
+ [:score [16 0]]
+
+
+ ;;
+ ]
