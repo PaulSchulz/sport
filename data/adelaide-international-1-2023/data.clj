09a3321e056dcd2d190f00a0437cca2f3f337822
Paul Schulz <paul@mawsonlakes.org>
2023-01-07 18:17:07 +1030

    adelaide-international-1-2023: Adds competition file
    Initial attempt at a tennis competition.

---
+++ data/adelaide-international-1-2023/data.clj
@@ -0,0 +1,50 @@
+{:event "adelaide-international-1-2023"
+ :data "data/adelaide-international-1-2023/data.clj",
+ :details
+ {:title "Adelaide International 1 2023",
+  :location "Adelaide",
+  :code :tennis,
+  :date "1 Jan 2023",
+  :url "https://atptour.com/en/scores/current/adelaide-1/",
+  ;; :fixtures "",
+  :datadir "data/adelaide-international-1-2023/",
+  :data "data/adelaide-international-1-2023/data.clj",
+  :custom "data/adelaide-international-1-2023/custom.clj"},
+ :players
+ [
+  {:draw  1 :name "Novak Djokovic"        :seed 1}
+  {:draw  2 :name "Constant Lestienne"}
+  {:draw  3 :name "Jordan Thompson"       :seed :wc}
+  {:draw  4 :name "Quentin Halys"}
+  {:draw  5 :name "Mikael Ymer"}
+  {:draw  6 :name "Roman Safiullin"       :seed :q}
+  {:draw  7 :name "Rinky Hijikata"        :seed :q}
+  {:draw  8:name "Denis Shapovalov"      :seed 7}
+
+  {:draw  9:name "Daniil Medvedev"       :seed 3}
+  {:draw 10 :name "Lorenzo Sonego"}
+  {:draw 11 :name "Miomir Kecmanovic"}
+  {:draw 12 :name "Christopher O'Connell" :seed :wc}
+  {:draw 13 :name "Soonwoo Kwon"          :seed :q}
+  {:draw 14 :name "Jack Draper"}
+  {:draw 15 :name "Pedro Cachin"}
+  {:draw 16 :name "Karen Khachanov"       :seed 8}
+
+  {:draw 17 :name "Jannik Sinner"         :seed 6}
+  {:draw 18 :name "Kyle Edmund"           :seed :pr}
+  {:draw 19 :name "Thanasi Kokkinakis"    :seed :wc}
+  {:draw 20 :name "Maxime Cressy"}
+  {:draw 21 :name "Andy Murray"}
+  {:draw 22 :name "Sebastian Korda"}
+  {:draw 23 :name "Roberto Bautista Agut"}
+  {:draw 24 :name "Andrey Rublev"         :seed 4}
+
+  {:draw 25 :name "Holger Rune"           :seed 5}
+  {:draw 26 :name "Yoshihito Nishioka"}
+  {:draw 27 :name "Mackenzie McDonald"}
+  {:draw 28 :name "Daniel Elahi Galan"}
+  {:draw 29 :name "Marcos Giron"}
+  {:draw 30 :name "Richard Gasquet"}
+  {:draw 31 :name "Alexei Popyrin"        :seed :q}
+  {:draw 32 :name "Felix Auger-Aliassime" :seed 2}
+  }
