53dbe5d241eba42bed9cef23926863b1f52a81e7
Paul Schulz <paul@mawsonlakes.org>
2023-01-08 19:16:09 +1030

    australian-open-2023: Adds data for preliminary tournaments

---
+++ data/australian-open-2023/data.clj
@@ -0,0 +1,310 @@
+{:event "australian-open-2023"
+ :data "data/australian-open-2023/data.clj",
+ :details
+ {:title "Australian Open 2023",
+  :location "Melbourne",
+  :code :tennis,
+  :date "1 Jan 2023",
+  :url "https://ausopen.com/",
+  ;; :fixtures "",
+  :datadir "data/australian-open-2023/",
+  :data "data/australian-open-2023/data.clj",
+  :custom "data/australian-open-2023/custom.clj"},
+ :draws
+ [{:id :mens-qualifying-singles
+   :name "Men's Qualifying Singles"
+   :draw
+   [{:draw  1 :name "A. Tabilo" :seed 1}
+    {:draw  2 :name "N. Milojevic"}
+    {:draw  3 :name "S. Ofner"}
+    {:draw  4 :name "F. Meligeni Alves"}
+    {:draw  5 :name "A. Bellier"}
+    {:draw  6 :name "L. Lokoli"}
+    {:draw  7 :name "L. Tu"}
+    {:draw  8 :name "J. Kovalik" :seed 25}
+
+    {:draw  9 :name "M. Cecchinato" :seed 2}
+    {:draw 10 :name "M. Purcell"}
+    {:draw 11 :name "D. Dzumhur"}
+    {:draw 12 :name "D. Kuzmanov"}
+    {:draw 13 :name "R. Olivo"}
+    {:draw 14 :name "A. Bolt"}
+    {:draw 15 :name "A. Ritschard"}
+    {:draw 16 :name "M. Arnaldi" :seed 22}
+
+    {:draw 17 :name "R. Albot" :seed 3}
+    {:draw 18 :name "B. Holt"}
+    {:draw 19 :name "G. Elias"}
+    {:draw 20 :name "K. Uchida"}
+    {:draw 21 :name "G. Blancaneaux"}
+    {:draw 22 :name "M. Trungelliti"}
+    {:draw 23 :name "M. Polmans"}
+    {:draw 24 :name "L. Klein" :seed 24}
+
+    {:draw 25 :name "E. Gomez" :seed  4}
+    {:draw 26 :name "L. Broady"}
+    {:draw 27 :name "J. Struff"}
+    {:draw 28 :name "A. Shevchenko"}
+    {:draw 29 :name "T. Schoolkate" :seed :wc}
+    {:draw 30 :name "K. Coppejans"}
+    {:draw 31 :name "R. Brancaccio"}
+    {:draw 32 :name "F. Agamenone" :seed 26}
+
+    {:draw 33 :name "J. Varillas" :seed 5}
+    {:draw 34 :name "R. Marcora"}
+    {:draw 35 :name "A. Galarneau"}
+    {:draw 36 :name "R. Peniston"}
+    {:draw 37 :name "M. Kukushkin"}
+    {:draw 38 :name "J. Duckworth"}
+    {:draw 39 :name "P. Sekulic" :seed :wc}
+    {:draw 40 :name "Y. Watanuki" :seed 30}
+
+    {:draw 41 :name "D. Kudla" :seed 6}
+    {:draw 42 :name "O. Jasika"}
+    {:draw 43 :name "A. Kovacevic"}
+    {:draw 44 :name "C. Taberner"}
+    {:draw 45 :name "S. Kozlov"}
+    {:draw 46 :name "O. Krutykh"}
+    {:draw 47 :name "G. Mager"}
+    {:draw 48 :name "F. Misolic" :seed 32}
+
+    {:draw 49 :name "H. Gaston" :seed 7}
+    {:draw 50 :name "E. Gerasimov"}
+    {:draw 51 :name "A. Muller"}
+    {:draw 52 :name "J. McCabe" :seed :wc}
+    {:draw 53 :name "E. Furness"}
+    {:draw 54 :name "Y. Hsu"}
+    {:draw 55 :name "N. Hardt"}
+    {:draw 56 :name "E. Ymer" :seed 18}
+
+    {:draw 57 :name "T. van Rijthoven" :seed 8}
+    {:draw 58 :name "L. Riedi"}
+    {:draw 59 :name "S. Rodriguez Taverna"}
+    {:draw 60 :name "G. Zeppieri"}
+    {:draw 61 :name "P. Jubb"}
+    {:draw 62 :name "D. Svrcina"}
+    {:draw 63 :name "N. Kicker"}
+    {:draw 64 :name "F. Delbonis" :seed 17}
+
+    {:draw 65 :name "M. Mmoh" :seed 9}
+    {:draw 66 :name "B. Kuzuhara" :seed :wc}
+    {:draw 67 :name "M. Pucinelli De Almeida"}
+    {:draw 68 :name "B. Paire"}
+    {:draw 69 :name "T. Tirante"}
+    {:draw 70 :name "F. Mena"}
+    {:draw 71 :name "G. Diallo"}
+    {:draw 72 :name "A. Vukic" :seed 21}
+
+    {:draw 73 :name "N. Gombos" :seed 10}
+    {:draw 74 :name "Y. Sugita"}
+    {:draw 75 :name "A. Vavassori"}
+    {:draw 76 :name "E. Nava"}
+    {:draw 77 :name "N. Jarry"}
+    {:draw 78 :name "O. Virtanen"}
+    {:draw 79 :name "N. Moreno De Alboran"}
+    {:draw 80 :name "T. Skatov" :seed 29}
+
+    {:draw 81 :name "P. Kotov" :seed 11}
+    {:draw 82 :name "G. Olivieri"}
+    {:draw 83 :name "H. Laaksonen"}
+    {:draw 84 :name "R. Bonadio"}
+    {:draw 85 :name "R. Noguchi"}
+    {:draw 86 :name "E. Winter" :seed :wc}
+    {:draw 87 :name "E. Escobedo"}
+    {:draw 88 :name "L. Nardi" :seed 23}
+
+    {:draw 89 :name "D. Stricker" :seed 12}
+    {:draw 90 :name "C. Stebe"}
+    {:draw 91 :name "M. Krueger"}
+    {:draw 92 :name "F. Cobolli"}
+    {:draw 93 :name "F. Ferreira Silva"}
+    {:draw 94 :name "B. Klahn"}
+    {:draw 95 :name "E. Couacaud"}
+    {:draw 96 :name "H. Grenier" :seed 28}
+
+    {:draw 97 :name "F. Passaro" :seed 13}
+    {:draw 98 :name "L. Rosol"}
+    {:draw 99 :name "T. Wu"}
+    {:draw 100 :name "M. Bellucci"}
+    {:draw 101 :name "L. Darderi"}
+    {:draw 102 :name "J. Ficovich"}
+    {:draw 103 :name "D. Sweeny" :seed :wc}
+    {:draw 104 :name "V. Kopriva" :seed 27}
+
+    {:draw 105 :name "P. Andujar" :seed 14}
+    {:draw 106 :name "A. Walton" :seed :wc}
+    {:draw 107 :name "D. Pham" :seed :wc}
+    {:draw 108 :name "P. Gojowczyk"}
+    {:draw 109 :name "F. Diaz Acosta"}
+    {:draw 110 :name "G. Brouwer"}
+    {:draw 111 :name "M. Marterer"}
+    {:draw 112 :name "Y. Han7mann" :seed 19}
+
+    {:draw 113 :name "J. Rodionov" :seed 15}
+    {:draw 114 :name "N. Ionel"}
+    {:draw 115 :name "F. Maestrelli"}
+    {:draw 116 :name "A. Andreev"}
+    {:draw 117 :name "M. Zekic"}
+    {:draw 118 :name "T. Barrios Vera"}
+    {:draw 119 :name "J. Jin" :seed :wc}
+    {:draw 120 :name "Z. Bergs" :seed 20}
+
+    {:draw 121 :name "F. Verdasco" :seed 16}
+    {:draw 122 :name "N. Serdarusic"}
+    {:draw 123 :name "F. Marozsan"}
+    {:draw 124 :name "J. Shang"}
+    {:draw 125 :name "A. Escoffier"}
+    {:draw 126 :name "Z. Piros"}
+    {:draw 127 :name "A. Pellegrino"}
+    {:draw 128 :name "M. Guinard" :seed 31}]}
+  ;;
+  {:id :womens-qualifying-singles
+  :name "Women's Qualifying Singles"
+   :draw
+   [{:draw  1 :name "A. Parks" :seed 1}
+    {:draw  2 :name "A. Bozovic" :seed :wc}
+    {:draw  3 :name "D. Astakhova"}
+    {:draw  4 :name "S. Bejlek"}
+    {:draw  5 :name "K. Zavatska"}
+    {:draw  6 :name "E. Vedder"}
+    {:draw  7 :name "B. Haas"}
+    {:draw  8 :name "E. Andreeva" :seed 29}
+
+    {:draw  9 :name "L. Noskova" :seed 2}
+    {:draw 10 :name "K. Sebov"}
+    {:draw 11 :name "P. Hon"}
+    {:draw 12 :name "K. Scott"}
+    {:draw 13 :name "A. Sharma" :seed :wc}
+    {:draw 14 :name "K. Hobgarski"}
+    {:draw 15 :name "G. Minneny"}
+    {:draw 16 :name "S. Waltert" :seed 20}
+
+    {:draw 17 :name "A. Schmiedlova" :seed 3}
+    {:draw 18 :name "I. Khromacheva"}
+    {:draw 19 :name "J. Ponchet"}
+    {:draw 20 :name "K. Swan"}
+    {:draw 21 :name "M. Zacarias"}
+    {:draw 22 :name "M. Hontama"}
+    {:draw 23 :name "S. Kartal"}
+    {:draw 24 :name "E. Mandlik" :seed 21}
+
+    {:draw 25 :name "C. Bucsa" :seed 4}
+    {:draw 26 :name "K. McPhee" :seed :wc}
+    {:draw 27 :name "A. Krueger"}
+    {:draw 28 :name "E. Bouchard"}
+    {:draw 29 :name "M. Bjorklund"}
+    {:draw 30 :name "E. Reyngold"}
+    {:draw 31 :name "L. Chirico"}
+    {:draw 32 :name "N. Hibino" :seed 32}
+
+    {:draw 33 :name "M. Frech" :seed 5}
+    {:draw 34 :name "S. Kraus"}
+    {:draw 35 :name "S. Vickery"}
+    {:draw 36 :name "K. Von Deichmann"}
+    {:draw 37 :name "A. Zakharova"}
+    {:draw 38 :name "L. Stefanini"}
+    {:draw 39 :name "G. Lee"}
+    {:draw 40 :name "R. Masarova" :seed 25}
+
+    {:draw 41 :name "A. Konjuh" :seed 6}
+    {:draw 42 :name "S. Chang"}
+    {:draw 43 :name "E. Avanesyan"}
+    {:draw 44 :name "L. Cabrera" :seed :wc}
+    {:draw 45 :name "O. Selekhmeteva"}
+    {:draw 46 :name "V. Diatchenko"}
+    {:draw 47 :name "R. Serban"}
+    {:draw 48 :name "A. Li" :seed 24}
+
+    {:draw 49 :name "S. Errani" :seed 7}
+    {:draw 50 :name "S. Jang"}
+    {:draw 51 :name "H. Baptiste"}
+    {:draw 52 :name "Y. Miyazaki"}
+    {:draw 53 :name "P. Marcinko"}
+    {:draw 54 :name "M. Barthel"}
+    {:draw 55 :name "A. Rodionova"}
+    {:draw 56 :name "C. Vandeweghe" :seed 23}
+
+    {:draw 57 :name "D. Shnaider" :seed 8}
+    {:draw 58 :name "J. Zuger"}
+    {:draw 59 :name "M. Carle"}
+    {:draw 60 :name "R. Montgomery"}
+    {:draw 61 :name "A. Tikhonova"}
+    {:draw 62 :name "S. Lamens"}
+    {:draw 63 :name "N. Han"}
+    {:draw 64 :name "V. Kuzmova" :seed 28}
+
+    {:draw 65 :name "L. Jeanjean" :seed 9}
+    {:draw 66 :name "I. Bara"}
+    {:draw 67 :name "K. Day"}
+    {:draw 68 :name "E. Makarova"}
+    {:draw 69 :name "A. Bolsova"}
+    {:draw 70 :name "H. Watson"}
+    {:draw 71 :name "Y. Naito"}
+    {:draw 72 :name "L. Tsurenko" :seed 31}
+
+    {:draw 73 :name "R. Jani" :seed 10}
+    {:draw 74 :name "D. Papamichail"}
+    {:draw 75 :name "C. Alves"}
+    {:draw 76 :name "O. Danilovic"}
+    {:draw 77 :name "C. Zhao"}
+    {:draw 78 :name "E. Chong"}
+    {:draw 79 :name "E. Jacquemot"}
+    {:draw 80 :name "C. Burel" :seed 17}
+
+    {:draw 81 :name "K. Volynets" :seed 11}
+    {:draw 82 :name "E. Gorgodze"}
+    {:draw 83 :name "L. Romero Gormaz"}
+    {:draw 84 :name "N. Abduraimova"}
+    {:draw 85 :name "K. Mladenovic"}
+    {:draw 86 :name "M. Inglis"}
+    {:draw 87 :name "V. Grammatikopoulou"}
+    {:draw 88 :name "M. Bassols Ribera" :seed 18}
+
+    {:draw 89 :name "A. Rus" :seed 12}
+    {:draw 90 :name "J. Bouzas Maneiro"}
+    {:draw 91 :name "E. Perez" :seed :wc}
+    {:draw 92 :name "P. Hule" :seed :wc}
+    {:draw 93 :name "F. Contreras Gomez"}
+    {:draw 94 :name "D. Snigur"}
+    {:draw 95 :name "N. Stevanovic"}
+    {:draw 96 :name "B. Fruhvirtova" :seed 27}
+
+    {:draw 97 :name "E. Ruse" :seed 13}
+    {:draw 98 :name "S. Mendez" :seed :wc}
+    {:draw 99 :name "R. Anderson"}
+    {:draw 100 :name "S. Janicijevic"}
+    {:draw 101 :name "B. Palicova"}
+    {:draw 102 :name "C. Paquet"}
+    {:draw 103 :name "E. Bolton" :seed :wc}
+    {:draw 104 :name "J. Burrage" :seed 26}
+
+    {:draw 105 :name "L. Pigossi" :seed 14}
+    {:draw 106 :name "M. Kempen"}
+    {:draw 107 :name "M. Doi"}
+    {:draw 108 :name "A. Eala"}
+    {:draw 109 :name "I. Oz"}
+    {:draw 110 :name "D. Aiava" :seed :wc}
+    {:draw 111 :name "A. Hartono"}
+    {:draw 112 :name "Y. In-Albon" :seed 19}
+
+    {:draw 113 :name "R. Peterson" :seed 15}
+    {:draw 114 :name "V. Jimenez Kasintseva"}
+    {:draw 115 :name "Z. Hives"}
+    {:draw 116 :name "H. Tan"}
+    {:draw 117 :name "C. Monnet"}
+    {:draw 118 :name "A. Cadantu-Ignatik"}
+    {:draw 119 :name "R. Vicens Mas"}
+    {:draw 120 :name "E. Lys" :seed 22}
+
+    {:draw 121 :name "A. Friedsam" :seed 16}
+    {:draw 122 :name "T. Wurth"}
+    {:draw 123 :name "P. Ormaechea"}
+    {:draw 124 :name "A. Muhammad"}
+    {:draw 125 :name "A. Gasanova"}
+    {:draw 126 :name "P. Kudermetova"}
+    {:draw 127 :name "C. Dolehide"}
+    {:draw 128 :name "K. Boulter" :seed 30}
+
+    ]
+   ;;
+  ]}
