d05e949ea49dd8b7e920145331f738585f4d76b1
Paul Schulz <paul@mawsonlakes.org>
2023-04-01 13:08:11 +1030

    Added round result file
    This is the first step in adding an 'update' data from an input file that
    contains 'change' information for match results.

---
+++ data/afl-2023/update-round-3.clj
@@ -0,0 +1,4 @@
+;; Code to update game results
+;; Round 3 - Matches 19-27
+(update-match-result 19 {:wb  "10.7(67)" :bri "7.11(53)"} "WB won by 14 points")
+(update-match-result 20 {:col "8.15(63)" :ric "7.7(49)"}  "COL won by 15 points")
